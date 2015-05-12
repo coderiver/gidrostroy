f90a1ca2dab35512ffab157177d7e9f5288768aa
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-05-12 18:23:06 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -3,9 +3,16 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+	
+	$('body').click(function(event){
+		$(".projects__info").removeClass('is-active');
+	});
 
-	$(".projects__marker").click(function(){
+	$(".projects__marker").click(function(event){
+		$(".projects__info").removeClass('is-active');
 		$(this).find(".projects__info").toggleClass("is-active");
+		event.preventDefault();
+		return false;
 	});
 
 });
