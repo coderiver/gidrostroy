aa061954dedefc10ed7579a6babf3e502ee35a4d
m-kozelchuk <marina.kozelchuk@gmail.com>
2015-05-13 13:58:24 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -3,6 +3,25 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
+
+	// contacts.html popup
+	$('.js-office').click(function(event) {
+		$('.overlay').show();
+		$('.popup_contacts').addClass('is-visible');
+		return false;
+	});
+
+	$('.js-person').click(function(event) {
+		$('.overlay').show();
+		$('.popup_contacts').addClass('is-visible');
+		return false;
+	});
+
+	$('.popup-close').click(function(event) {
+		$('.overlay').hide();
+		$('.popup').removeClass('is-visible');
+		return false;
+	});
 	
 	// project.html slider
 	$('.slider-for').slick({
@@ -13,11 +32,11 @@ head.ready(function() {
 		asNavFor: '.slider-nav'
 	});
 	$('.slider-nav').slick({
-		slidesToShow: 3,
+		slidesToShow: 4,
 		slidesToScroll: 1,
 		asNavFor: '.slider-for',
 		dots: false,
-		centerMode: true
+		focusOnSelect: true
 	});
 
 	// projects.html
